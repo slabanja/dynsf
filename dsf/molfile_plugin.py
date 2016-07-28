98f4aa0aaec6a8bb899300830ebb456f76eeaf95
mattias <mattias.slabanja@gmail.com>
2016-07-28 16:30:34 +0200

    Tentative, make molfileplugin work with windows

--- dsf/molfile_plugin.py
+++ dsf/molfile_plugin.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import re
 
 # Copyright (C) 2011 Mattias Slabanja <slabanja@chalmers.se>
 #
@@ -17,34 +18,35 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 # 02110-1301, USA.
 
-__all__ = ['MolfilePlugin', 'TRAJECTORY_PLUGIN_MAPPING',
-           'molfile_timestep_t', 'molfile_timestep_metadata_t',
-           'molfile_atom_t', 'molfile_plugin_t']
-
-__doc__ = """Molfile plugin interface
+"""Molfile plugin interface
 
 Not much more than the necessary ctypes stuff.
 """
 
-import os
-try:
-    from sysconfig import get_config_var
-except ImportError:
-    # Fallback for python before version 2.7
-    from distutils.sysconfig import get_config_var
+__all__ = ['MolfilePlugin', 'TRAJECTORY_PLUGIN_MAPPING',
+           'molfile_timestep_t', 'molfile_timestep_metadata_t',
+           'molfile_atom_t', 'molfile_plugin_t']
 
+
+import os
+from sys import maxsize
+from platform import uname
 from itertools import islice
 
 from ctypes import (
     cdll, CDLL, RTLD_GLOBAL,
     POINTER as PTR, CFUNCTYPE as CFT,
-    Structure, cast, pointer, byref,
+    Structure, cast, pointer,
     c_int, c_uint, c_float, c_double, c_char, c_char_p, c_void_p)
 from ctypes.util import find_library
 
-# Maybe a bit ugly. Is there a more kosher way of making sure that the
-# libstdc++ (or similar) symbols are available to the molfile-plugins?
-_cxx = CDLL(find_library('stdc++'), mode=RTLD_GLOBAL)
+_system = uname()[0]
+_64bit_python = maxsize > 2 ** 32
+if _system != "Windows":
+    # a bit ugly. Is there a more kosher way of making sure that the
+    # libstdc++ (or similar) symbols are available to the molfile-plugins?
+    _cxx = CDLL(find_library('stdc++'), mode=RTLD_GLOBAL)
+
 
 c_int_p = PTR(c_int)
 c_int_pp = PTR(c_int_p)
@@ -75,61 +77,53 @@ c_float_pp = PTR(c_float_p)
 #
 
 TRAJECTORY_PLUGIN_MAPPING = (
-    #(SOFTWARE-NAME, FILE-TYPE, FILE-SUFFIX, PLUGIN-NAME)
-    ('AMBER',    '"binpos"',          'binpos',    'binposplugin'),
-    ('AMBER',    '"CRD"',             'crd',       'crdplugin'),
-    ('AMBER',    'NetCDF',            'nc',        'netcdfplugin'),
-    ('CHARMM',   '"DCD" - CHARMM, NAMD, XPLOR',
-                                      'dcd',       'dcdplugin'),
-    ('CPMD',     'CPMD',              'cpmd',      'cpmdplugin'),
-    ('DLPOLY',   'DLPOLY History',    'dlpolyhist','dlpolyplugin'),
-    ('GROMACS',  'Gromacs XTC',       'xtc',       'gromacsplugin'),
-    ('GROMACS',  'Gromacs TRR',       'trr',       'gromacsplugin'),
-    ('LAMMPS',   'LAMMPS Trajectory', 'lammpstrj', 'lammpsplugin'),
-    ('VASP',     'VASP ionic steps',  'xml',       'vaspxmlplugin'),
-    ('VASP',     'VASP ionic steps',  'OUTCAR',    'vaspoutcarplugin'),
-    ('VTF',      'VTF trajectory',    'vtf',       'vtfplugin'),
-    ('XCrySDen', 'XSF trajectory',    'xsf',       'xsfplugin'),
-    ('XCrySDen', 'AXSF trajectory',   'axsf',      'xsfplugin'),
-    ('?',        'XYZ trajectory',    'xyz',       'xyzplugin'))
+    # (SOFTWARE-NAME, FILE-TYPE, FILE-SUFFIX, PLUGIN-NAME)
+    ('AMBER', '"binpos"',
+     'binpos', 'binposplugin'),
+
+    ('AMBER', '"CRD"',
+     'crd', 'crdplugin'),
+
+    ('AMBER', 'NetCDF',
+     'nc', 'netcdfplugin'),
+
+    ('CHARMM', '"DCD" - CHARMM, NAMD, XPLOR',
+     'dcd', 'dcdplugin'),
+
+    ('CPMD', 'CPMD',
+     'cpmd', 'cpmdplugin'),
+
+    ('DLPOLY', 'DLPOLY History',
+     'dlpolyhist', 'dlpolyplugin'),
+
+    ('GROMACS', 'Gromacs XTC',
+     'xtc', 'gromacsplugin'),
+
+    ('GROMACS', 'Gromacs TRR',
+     'trr', 'gromacsplugin'),
+
+    ('LAMMPS', 'LAMMPS Trajectory',
+     'lammpstrj', 'lammpsplugin'),
+
+    ('VASP', 'VASP ionic steps',
+     'xml', 'vaspxmlplugin'),
+
+    ('VASP', 'VASP ionic steps',
+     'OUTCAR', 'vaspoutcarplugin'),
+
+    ('VTF', 'VTF trajectory',
+     'vtf', 'vtfplugin'),
+
+    ('XCrySDen', 'XSF trajectory',
+      'xsf', 'xsfplugin'),
+
+    ('XCrySDen', 'AXSF trajectory',
+      'axsf', 'xsfplugin'),
+
+    ('?', 'XYZ trajectory',
+     'xyz', 'xyzplugin'))
 
 
-
-
-def find_molfile_plugin_dir():
-    # somewhat lengthyish way of finding the plugins
-    from platform import uname
-
-    vmddir = None
-    system,_,_,_,machine,_ = uname()
-    rel_path = 'plugins/%s/molfile' % {
-        ('Linux', 'x86_64') : 'LINUXAMD64',
-        ('Linux', 'i386') : 'LINUX',
-        #('Darwin', 'x86_64') : 'MACOSXX86',
-        ('Darwin', 'i386') : 'MACOSXX86'
-        }.get((system, machine), 'UNKNOWN')
-
-    if 'VMDDIR' in os.environ:
-        vmddir = os.environ['VMDDIR']
-    else:
-        import re
-        for d in os.environ['PATH'].split(os.pathsep):
-            f = os.path.join(d, 'vmd')
-            if os.path.exists(f):
-                with open(f, 'r') as fh:
-                    for L in islice(fh, 10):
-                        m = re.match(r'^(?:set )?defaultvmddir=(?:"(/.*)"|(/[^# ]*)).*$', L)
-                        if m:
-                            a,b = m.groups()
-                            vmddir = a or b
-    if vmddir:
-        x = os.path.join(vmddir, rel_path)
-        if os.path.exists(x):
-            return x
-    return None
-
-
-MOLFILE_PLUGIN_DIR = find_molfile_plugin_dir()
 MIN_ABI_VERSION = 15
 
 MOLFILE_PLUGIN_TYPE = "mol file reader"
@@ -146,22 +140,22 @@ class vmdplugin_t(Structure):
                 ('is_reentrant', c_int)]
 
 class molfile_metadata_t(Structure):
-    _fields_ = [('database', c_char*81),
-                ('accession', c_char*81),
-                ('date', c_char*81),
-                ('title', c_char*81),
+    _fields_ = [('database', c_char * 81),
+                ('accession', c_char * 81),
+                ('date', c_char * 81),
+                ('title', c_char * 81),
                 ('remarklen', c_int),
                 ('remarks', c_char_p)]
 
 class molfile_atom_t(Structure):
-    _fields_ = [('name', c_char*16),
-                ('type', c_char*16),
-                ('resname', c_char*16),
+    _fields_ = [('name', c_char * 16),
+                ('type', c_char * 16),
+                ('resname', c_char * 16),
                 ('resid', c_int),
-                ('segid', c_char*16),
-                ('chain', c_char*16),
-                ('altloc', c_char*16),
-                ('insertion', c_char*16),
+                ('segid', c_char * 16),
+                ('chain', c_char * 16),
+                ('altloc', c_char * 16),
+                ('insertion', c_char * 16),
                 ('occupancy', c_float),
                 ('bfactor', c_float),
                 ('mass', c_float),
@@ -194,11 +188,11 @@ class molfile_timestep_t(Structure):
                 ('physical_time', c_double)]
 
 class molfile_volumetric_t(Structure):
-    _fields_ = [('dataname', c_char*256),
-                ('origin', c_float*3),
-                ('xaxis', c_float*3),
-                ('yaxis', c_float*3),
-                ('zaxis', c_float*3),
+    _fields_ = [('dataname', c_char * 256),
+                ('origin', c_float * 3),
+                ('xaxis', c_float * 3),
+                ('yaxis', c_float * 3),
+                ('zaxis', c_float * 3),
                 ('xsize', c_int),
                 ('ysize', c_int),
                 ('zsize', c_int),
@@ -338,28 +332,23 @@ class MolfilePlugin:
 
     A call to the class method 'close', calls the plugin fini-function.
     """
-    def __init__(self, plugin_name, plugin_dir=MOLFILE_PLUGIN_DIR):
+    def __init__(self, plugin_name, plugin_dir=None):
 
-        if plugin_dir is None:
-            raise RuntimeError("The provided plugindir is None, "\
-                               "couldn't find any plugins. "\
-                               "Do you have vmd in your PATH, or is VMDDIR "\
-                               "correctly set?")
-
-        lib = cdll.LoadLibrary(os.path.join(plugin_dir,
-                                            plugin_name+get_config_var('SO')))
+        self._plugin_dir = self._find_molfile_plugin_dir(plugin_dir)
+        lib = self._molfile_plugin_lib(plugin_name)
 
         # extern int vmdplugin_init(void);
-        # extern int vmdplugin_fini(void);
-        # extern int vmdplugin_register(void *, vmdplugin_register_cb);
-
         lib.vmdplugin_init.restype = c_int
+
+        # extern int vmdplugin_fini(void);
         lib.vmdplugin_fini.restype = c_int
+
+        # extern int vmdplugin_register(void *, vmdplugin_register_cb);
         lib.vmdplugin_register.restype = c_int
         lib.vmdplugin_register.argtypes = (c_void_p, vmdplugin_register_cb_t)
 
         plugin_p = pointer(molfile_plugin_t(0))
-        def py_reg_cb(v, p):
+        def py_reg_callback(v, p):
             pc = p.contents
             if pc.type == MOLFILE_PLUGIN_TYPE:
                 if pc.abiversion >= MIN_ABI_VERSION:
@@ -369,7 +358,7 @@ class MolfilePlugin:
         if lib.vmdplugin_init() != VMDPLUGIN_SUCCESS:
             raise RuntimeError('Failed to init %s' % plugin_name)
 
-        vmdplugin_register_cb = vmdplugin_register_cb_t(py_reg_cb)
+        vmdplugin_register_cb = vmdplugin_register_cb_t(py_reg_callback)
         if lib.vmdplugin_register(None, vmdplugin_register_cb) != VMDPLUGIN_SUCCESS:
             raise RuntimeError('Failed to register %s' % plugin_name)
 
@@ -380,11 +369,76 @@ class MolfilePlugin:
         self.plugin = molfile_plugin_t(0)
         self._lib.vmdplugin_fini()
 
+    def _molfile_plugin_lib(self, plugin_name):
+        if self._plugin_dir is None:
+            raise RuntimeError("No molfile plugin dir found. Consider setting evironment "
+                               "variable VMDDIR and use a python interpreted with matching "
+                               "architecture as molfile libraries.")
+
+        lib_path = os.path.join(self._plugin_dir,
+                                plugin_name + '.' + self._molfile_machine_specific_plugin_suffix())
+
+        return cdll.LoadLibrary(lib_path)
+
+    def _find_molfile_plugin_dir(self, plugin_dir):
+        if plugin_dir is not None and os.path.exists(plugin_dir):
+            return plugin_dir
+
+        vmddir = os.environ.get('VMDDIR')
+        if vmddir is not None:
+            return os.path.join(vmddir,
+                                self._molfile_relative_plugin_dir())
+
+        for path in os.environ['PATH'].split(os.pathsep):
+            tentative_vmd_script_path = os.path.join(path, 'vmd')
+            if os.path.exists(tentative_vmd_script_path):
+                vmddir = self._get_vmddir_from_vmd_script(tentative_vmd_script_path)
+                if vmddir is not None:
+                    return os.path.join(vmddir,
+                                        self._molfile_relative_plugin_dir())
+
+    def _get_vmddir_from_vmd_script(self, vmd_script_path):
+        with open(vmd_script_path) as fh:
+            for L in islice(fh, 10):
+                m = re.match(r'^(?:set )?defaultvmddir=(?:"(/.*)"|(/[^# ]*)).*$', L)
+                if m:
+                    a, b = m.groups()
+                    return a or b
+
+    def _molfile_relative_plugin_dir(self):
+        return os.path.join('plugins',
+                            self._molfile_machine_specific_plugin_dir_name(),
+                            'molfile')
+
+    def _molfile_machine_specific_plugin_suffix(self):
+        return 'so'
+
+    def _molfile_machine_specific_plugin_dir_name(self):
+        if _system == 'Linux':
+            if _64bit_python:
+                return 'LINUXAMD64'
+            else:
+                return 'LINUX'
+
+        elif not _64bit_python:
+            if _system == 'Darwin':
+                return 'MACOSXX86'
+
+            elif _system == 'Windows':
+                return 'WIN32'
+
+        bits = "64" if _64bit_python else "32"
+        raise RuntimeError("No known %s-bit plugin-path on %s system" % (bits, _system))
 
 
 if __name__ == '__main__':
-    for _,_,ext,pn in TRAJECTORY_PLUGIN_MAPPING:
-        p = MolfilePlugin(pn)
+    for _, _, _, pn in TRAJECTORY_PLUGIN_MAPPING:
+        try:
+            p = MolfilePlugin(pn)
+        except:
+            print "Failed opening %s" % pn
+            continue
+
         print("%-20s %-15s (%5s %5s %5s %5s)" % \
                   (pn, p.plugin.filename_extension,
                    bool(p.plugin.read_timestep),
